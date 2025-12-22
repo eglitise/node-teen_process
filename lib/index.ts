e36d5ba32379e36ff7e847fb5e8871db857f4b8f
Edgars Eglītis <edgars.eglitis@outlook.com>
2025-12-22 11:03:27 +0200

    fix: add back default export for compatibility

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,17 @@
-export {spawn} from 'node:child_process';
-export {SubProcess} from './subprocess';
-export {exec} from './exec';
+import {spawn} from 'node:child_process';
+import {SubProcess} from './subprocess';
+import {exec} from './exec';
+
+export {spawn, SubProcess, exec};
 export type {
+  TeenProcess,
   TeenProcessExecOptions,
   TeenProcessExecResult,
   ExecError,
   SubProcessOptions
 } from './types';
+export default {
+  spawn,
+  SubProcess,
+  exec
+};
